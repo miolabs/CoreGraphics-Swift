1052c2d81ae2d0daa3850813df9ca39ce9e2eb4b
Javier Segura <javier@miolabs.com>
2022-04-10 17:07:09 +0100

    working on cgfloat

--- Sources/CoreGraphics/CGFloat.swift
+++ Sources/CoreGraphics/CGFloat.swift
@@ -11,24 +11,24 @@ import Foundation
 //@frozen
 struct CGFloat : Equatable
 {
-    var value: Double = 0
+    var v: Double = 0
     
     ///Create an instance initialized to zero.
     init() {}
     
     ///value rounded to the closest representable Self.
     init( _ value: Float) {
-        self.value = Double( value )
+        v = Double( value )
     }
     
     ///value rounded to the closest representable Self.
     init( _ value: Double) {
-        self.value = value
+        v = value
     }
     
     ///Create an instance initialized to value.
     init( _ value: CGFloat) {
-        self.value = value.value
+        v = value.v
     }
     
 }
